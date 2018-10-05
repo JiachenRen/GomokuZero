278f7ea378328e6675fa876961137629d9e0d899
jiachenren <arc4jiachenren@gmail.com>
2018-10-05 18:54:39 -0400

    Initial Commit

---
+++ Gomoku Zero/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Gomoku Zero
+//
+//  Created by Jiachen Ren on 10/5/18.
+//  Copyright © 2018 Jiachen Ren. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
